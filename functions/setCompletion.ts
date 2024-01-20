53037d951f99ce4a6ff4d5161837e07cab5022dd
Kirill <xakdog@gmail.com>
2024-01-20 12:27:46 +0100

    Initial commit

---
+++ functions/setCompletion.ts
@@ -0,0 +1,27 @@
+import { models, permissions, SetCompletion, Todo } from "@teamkeel/sdk";
+
+export default SetCompletion({
+  beforeQuery: async (ctx, inputs) => {
+    const now = new Date();
+
+    const todo = await models.todo.findOne(inputs.where);
+
+    if (todo && todo.ownerId == ctx.identity?.id) {
+      permissions.allow();
+    } else {
+      permissions.deny();
+    }
+
+    const values: Partial<Todo> = {
+      ...inputs.values,
+    };
+
+    if (inputs.values.complete) {
+      values.completedAt = now;
+    }
+
+    return models.todo.update(inputs.where, {
+      ...values,
+    });
+  },
+});
